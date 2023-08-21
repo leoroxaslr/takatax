599f9656716fd0fa2588edb1aa547d227f154796
leoroxaslr <leoroxas.lr@gmail.com>
2023-08-21 22:37:08 +0800

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // darkMode: 'class',
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  colors: {
+    white: "#fffff",
+    blue: "#1fb6ff",
+    purple: "#7e5bef",
+    pink: "#ff49db",
+    yellow: "#ffc82c",
+    "gray-dark": "#273444",
+    gray: "#8492a6",
+    "gray-light": "#d3dce6",
+  },
+  plugins: [],
+};
