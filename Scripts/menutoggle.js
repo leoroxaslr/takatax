e6466ff5b09e0a9d2b37b548d24b59fab3b7e0bb
leoroxaslr <leoroxas.lr@gmail.com>
2023-08-24 00:15:03 +0800

    updated menu toggle

---
+++ Scripts/menutoggle.js
@@ -0,0 +1,9 @@
+function myFunction(x) {
+  var x = document.getElementById("navbar-default");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
